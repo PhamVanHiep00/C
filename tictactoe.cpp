2aa288f07c2b014e53632d12235b73ec7fef822f
PhamVanHiep00 <160395667+PhamVanHiep00@users.noreply.github.com>
2024-02-18 17:53:31 +0700

    Add files via upload

---
+++ tictactoe.cpp
@@ -0,0 +1,126 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+
+void print(char **matrix){
+	for( int i = 0; i < 3; i++){
+		for( int j = 0; j < 3; j++){
+			printf("%c|", matrix[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+void computerChoice(char **matrix){
+	int a = 0,b = 0,c = 0;
+	for( int i = 0; i < 3; i++){
+		for( int j = 0; j < 3; j++){
+			if( i == 0){if(matrix[i][j] == ' '){a++;}}
+			if( i == 1){if(matrix[i][j] == ' '){b++;}}
+			if( i == 2){if(matrix[i][j] == ' '){c++;}}
+		}
+	}
+	if ( a > 0){
+		for( int i = 0; i < 3; i++){
+			if(matrix[0][i] == ' '){
+				matrix[0][i] = 'o';
+				break;
+			}
+		}
+	}else{
+		if( b > 0){
+			for( int i = 0; i < 3; i++){
+				if(matrix[1][i] == ' '){
+					matrix[1][i] = 'o';
+					break;
+				}
+			}
+		}else{
+			if( c > 0){
+				for( int i = 0; i < 3; i++){
+					if(matrix[2][i] == ' '){
+						matrix[2][i] = 'o';
+						break;
+					}
+				}
+			}
+		}
+	}
+}
+
+int checkTest( char **matrix, char choice){
+	if( matrix[0][0] == choice && matrix[0][1] == choice && matrix[0][2] == choice){return -1;}
+	if( matrix[1][0] == choice && matrix[1][1] == choice && matrix[1][2] == choice){return -1;}
+	if( matrix[2][0] == choice && matrix[2][1] == choice && matrix[2][2] == choice){return -1;}
+	if( matrix[0][1] == choice && matrix[1][1] == choice && matrix[2][1] == choice){return -1;}
+	if( matrix[0][2] == choice && matrix[1][2] == choice && matrix[2][2] == choice){return -1;}
+	if( matrix[0][0] == choice && matrix[1][0] == choice && matrix[2][0] == choice){return -1;}
+	if( matrix[0][0] == choice && matrix[1][1] == choice && matrix[2][2] == choice){return -1;}
+	if( matrix[0][2] == choice && matrix[1][1] == choice && matrix[2][0] == choice){return -1;}
+}
+	
+int check(char **matrix){
+	char choiceX = 'x';
+	char choiceO = 'o';
+	if (checkTest(matrix,choiceX) == -1){return 1;}
+	if (checkTest(matrix,choiceO) == -1){return 2;}
+	int k = 0;
+	for( int i = 0; i < 3; i++){
+		for( int j = 0; j < 3; j++){
+			if (matrix[i][j] != ' '){
+				k++;
+			}
+		}
+	}
+	if( k == 9){
+		return 0;
+	}
+	return 3;
+}
+	
+	//return 3 = continue
+	//return 2 = computerWin
+	//return 1 = humanWin
+	//return 0 = Draw
+	
+int main(){
+	//heading
+	char **matrix;
+		// cap phat bo nho //
+	matrix = (char **)malloc(3 * sizeof(char *));
+    for (int i = 0; i < 3; i++) {
+        matrix[i] = (char *)malloc(3 * sizeof(char));
+    }
+
+    for( int i = 0; i < 3; i++){
+		for( int j = 0; j < 3; j++){
+			matrix[i][j] = ' '; 
+		}
+	}
+    for( int i = 0; i < 3; i++){
+		for( int j = 0; j < 3; j++){
+			printf("%c|", matrix[i][j]);
+		}
+		printf("\n");
+	}
+	char choiceX = 'x';
+	char choiceO = 'o';
+	//gamePlay
+	while(1){
+		int m,n;
+		printf("YourChoice: ");scanf("%i%i",&m,&n);
+		m = m-1;
+		n = n-1;
+		// choice X
+		matrix[m][n] = 'x';
+		// choice O	
+		computerChoice(matrix);
+		print(matrix);
+			
+		// endGame
+		if(check(matrix) == 0){printf("Draw");break;}
+		if(check(matrix) == 1){printf("Win");break;}
+		if(check(matrix) == 2){printf("Lost");break;}
+	}	
+}
